2a9f132233d20b042dded69c583581067df3b8dc
Pekky29 <35894246+Pekky29@users.noreply.github.com>
2018-02-14 15:39:23 +0700

    เพิ่ม sql ไฟล์สำหรับสร้าง data helloworld

---
+++ Helloworld.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE HelloWorld (
+            idName int,
+            name varchar(255),
+	    primary key (idName) 
+            ); 
+INSERT INTO HelloWorld(idName,name)
+            VALUES (1,"HELLOWORLD");
